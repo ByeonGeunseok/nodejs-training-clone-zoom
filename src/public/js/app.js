b9cd9cae4aaec6952286bd16339359248722228b
Geunseok Byeon <byeongeunseok@gmail.com>
2023-06-15 20:42:50 +0900

    send messages between each other

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -14,11 +14,23 @@ function addMessage(message) {
   li.innerText = message;
   ul.appendChild(li);
 }
+
+function handleMessageSubmit(event) {
+  event.preventDefault();
+  const input = room.querySelector("input");
+  socket.emit("new_message", input.value, roomName, () => {
+    addMessage(`You: ${input.value}`);
+    input.value = "";
+  });
+}
+
 function showRoom() {
   welcome.hidden = true;
   room.hidden = false;
   const h3 = room.querySelector("h3");
   h3.innerText = `Room ${roomName}`;
+  const form = room.querySelector("form");
+  form.addEventListener("submit", handleMessageSubmit);
 }
 function handleRoomSubmit(event) {
   event.preventDefault();
@@ -37,3 +49,5 @@ socket.on("welcome", () => {
 socket.on("bye", () => {
   addMessage("someone left.");
 });
+
+socket.on("new_message", addMessage);

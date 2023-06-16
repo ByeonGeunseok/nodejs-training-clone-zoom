bff9abd16139ff6df57aa49d5573fb64e8c07bb3
Geunseok Byeon <byeongeunseok@gmail.com>
2023-06-16 23:37:07 +0900

    implement localtunnel and STUN server integration

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -141,7 +141,20 @@ socket.on("ice", (ice) => {
 });
 // RTC Code
 function makeConnection() {
-  myPeerConnection = new RTCPeerConnection();
+  myPeerConnection = new RTCPeerConnection({
+    iceServers: [
+      {
+        urls: [
+          // google stun server. only use for develop.
+          // "stun:stun.l.google.com:19302",
+          // "stun:stun1.l.google.com:19302",
+          // "stun:stun2.l.google.com:19302",
+          // "stun:stun3.l.google.com:19302",
+          // "stun:stun4.l.google.com:19302",
+        ],
+      },
+    ],
+  });
   myPeerConnection.addEventListener("icecandidate", handleIce);
   myPeerConnection.addEventListener("addstream", handleAddStream);
   myStream.getTracks().forEach((track) => myPeerConnection.addTrack(track, myStream));

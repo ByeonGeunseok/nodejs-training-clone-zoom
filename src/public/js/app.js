2ecf0b9c8c705e32891735ff4d36dc2c73cb57df
Geunseok Byeon <byeongeunseok@gmail.com>
2023-06-17 00:00:35 +0900

    mask STUN server address

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -154,11 +154,11 @@ function makeConnection() {
       {
         urls: [
           // google stun server. only use for develop.
-          "stun:stun.l.google.com:19302",
-          "stun:stun1.l.google.com:19302",
-          "stun:stun2.l.google.com:19302",
-          "stun:stun3.l.google.com:19302",
-          "stun:stun4.l.google.com:19302",
+          // "stun:stun.l.google.com:19302",
+          // "stun:stun1.l.google.com:19302",
+          // "stun:stun2.l.google.com:19302",
+          // "stun:stun3.l.google.com:19302",
+          // "stun:stun4.l.google.com:19302",
         ],
       },
     ],
